1104c7ab5e7c9ba13d80da1b06684f01a798be0e
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2018-12-31 15:33:05 +0900

    feat: add Thread2_UserProcessingFunction

---
+++ examples/TutorialDeveloper/Thread2_UserProcessingFunction/README.md
@@ -0,0 +1,25 @@
+# Thread 2 UserProcessing Function
+
+## Quick Start
+
+#### 1. Build OpenPose
+
+````dos
+> cd <OpenPoseDotNet_dir>
+> BuildWindowsVS2015.bat <Debug/Release>
+````
+
+#### 2. Build OpenPoseDotNet.Native
+
+````dos
+> cd <OpenPoseDotNet_dir>\src\OpenPoseDotNet.Native
+> BuildWindowsVS2015.bat <Debug/Release>
+````
+
+#### 3. Try Tutorial
+
+````dos
+> cd <OpenPoseDotNet_dir>\examples\TutorialDeveloper\Thread2_UserProcessingFunction
+> CopyOpenPose.bat <Debug/Release>
+> dotnet run -c Release
+````
---
+++ examples/TutorialDeveloper/Thread2_UserProcessingFunction/WUserClass.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using OpenPoseDotNet;
+
+namespace Thread2_UserProcessingFunction
+{
+
+    // This class can be implemented either as a template or as a simple class given
+    // that the user usually knows which kind of data he will move between the queues,
+    // in this case we assume a std::shared_ptr of a std::vector of op::Datum
+    internal sealed class WUserClass : UserWorker<Datum>
+    {
+
+        #region Constructors
+
+        public WUserClass():
+            base()
+        {
+            // User's constructor here
+        }
+
+        #endregion
+
+        #region Methods
+
+        protected override void InitializationOnThread()
+        {
+        }
+
+        protected override void Work(Datum[] datumsPtr)
+        {
+            try
+            {
+                // User's processing here
+                // datum.cvInputData: initial cv::Mat obtained from the frames producer (video, webcam, etc.)
+                // datum.cvOutputData: final cv::Mat to be displayed
+                if (datumsPtr != null)
+                    foreach (var datum in datumsPtr)
+                        Cv.BitwiseNot(datum.CvInputData, datum.CvOutputData);
+            }
+            catch (Exception e)
+            {
+                OpenPose.Log("Some kind of unexpected error happened.");
+                this.Stop();
+                OpenPose.Error(e.Message, -1, nameof(this.Work));
+            }
+        }
+
+        #endregion
+
+    }
+
+}
