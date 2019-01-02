d863f17234ae5e7db91c9e4166fc49a5b1835316
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2019-01-03 00:22:00 +0900

    fix: comile error by gcc

--- src/OpenPoseDotNet.Native/openpose/defines.h
+++ src/OpenPoseDotNet.Native/openpose/defines.h
@@ -7,16 +7,16 @@
 #pragma region template
 
 #define MAKE_DEFINE(__TYPE__, __TYPENAME__)\
-typedef std::vector<__TYPE__> ##__TYPENAME__##DatumsNoPtr;\
-typedef std::shared_ptr<##__TYPENAME__##DatumsNoPtr> ##__TYPENAME__##Datums;\
-typedef op::WrapperT<##__TYPENAME__##DatumsNoPtr> ##__TYPENAME__##Wrapper;\
-typedef op::ThreadManager<##__TYPENAME__##Datums> ##__TYPENAME__##ThreadManager;\
-typedef std::shared_ptr<op::Worker<##__TYPENAME__##Datums>> ##__TYPENAME__##Worker;\
-typedef op::DatumProducer<##__TYPENAME__##DatumsNoPtr> ##__TYPENAME__##DatumProducer;\
-typedef op::WDatumProducer<##__TYPENAME__##Datums, ##__TYPENAME__##DatumsNoPtr> ##__TYPENAME__##WDatumProducer;\
-typedef op::WGui<##__TYPENAME__##Datums> ##__TYPENAME__##WGui;\
-typedef op::WorkerConsumer<##__TYPENAME__##Datums> ##__TYPENAME__##WorkerConsumer;\
-typedef op::WorkerProducer<##__TYPENAME__##Datums> ##__TYPENAME__##WorkerProducer;\
+typedef std::vector<__TYPE__> __TYPENAME__##DatumsNoPtr;\
+typedef std::shared_ptr<__TYPENAME__##DatumsNoPtr> __TYPENAME__##Datums;\
+typedef op::WrapperT<__TYPENAME__##DatumsNoPtr> __TYPENAME__##Wrapper;\
+typedef op::ThreadManager<__TYPENAME__##Datums> __TYPENAME__##ThreadManager;\
+typedef std::shared_ptr<op::Worker<__TYPENAME__##Datums>> __TYPENAME__##Worker;\
+typedef op::DatumProducer<__TYPENAME__##DatumsNoPtr> __TYPENAME__##DatumProducer;\
+typedef op::WDatumProducer<__TYPENAME__##Datums, __TYPENAME__##DatumsNoPtr> __TYPENAME__##WDatumProducer;\
+typedef op::WGui<__TYPENAME__##Datums> __TYPENAME__##WGui;\
+typedef op::WorkerConsumer<__TYPENAME__##Datums> __TYPENAME__##WorkerConsumer;\
+typedef op::WorkerProducer<__TYPENAME__##Datums> __TYPENAME__##WorkerProducer;\
 
 #pragma endregion template
 
