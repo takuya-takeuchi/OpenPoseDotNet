a8e953ec3eb559e3817be618fba1915f71715537
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2020-06-14 04:27:53 +0900

    feat: add hand pose position

---
+++ src/OpenPoseDotNet/Pose/PoseHandPart.cs
@@ -0,0 +1,54 @@
+﻿// ReSharper disable once CheckNamespace
+namespace OpenPoseDotNet
+{
+
+    public enum PoseHandPart
+    {
+
+        // ToDo: There is more better name
+        //       https://github.com/CMU-Perceptual-Computing-Lab/openpose/blob/master/doc/output.md#hand-output-format
+        Wrist = 0,
+
+        Thumb1Carpometacarpal,
+
+        Thumb2Knuckles,
+
+        Thumb3Interphalangeal,
+
+        Thumb4FingerTip,
+
+        Index1Knuckles,
+
+        Index2ProximalInterphalangeal,
+
+        Index3DistalInterphalangeal,
+
+        Index4FingerTip,
+
+        Middle1Knuckles,
+
+        Middle2ProximalInterphalangeal,
+
+        Middle3DistalInterphalangeal,
+
+        Middle4FingerTip,
+
+        Ring1Knuckles,
+
+        Ring2ProximalInterphalangeal,
+
+        Ring3DistalInterphalangeal,
+
+        Ring4FingerTip,
+
+        Pinky1Knuckles,
+
+        Pinky2ProximalInterphalangeal,
+
+        Pinky3DistalInterphalangeal,
+
+        Pinky4FingerTip
+
+    }
+
+}
